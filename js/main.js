484802b86138c1df19a270fe66d7f923fdcfc597
marlinfishkayu <69189573+marlinfishkayu@users.noreply.github.com>
2020-08-14 16:12:34 +0800

    update

--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@
 // 	})
 // })();
 
+//new hamburger 
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen){
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else{
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+
+
 
 //navbar scroll add class
 $(window).on('scroll', function(){
