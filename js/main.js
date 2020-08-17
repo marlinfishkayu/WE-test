d35525e84c728392143a58ed797b6086080cd956
marlinfishkayu <69189573+marlinfishkayu@users.noreply.github.com>
2020-08-17 16:40:08 +0800

    up

--- js/main.js
+++ js/main.js
@@ -4,34 +4,66 @@
 	})
 })();
 
+
+
+/*-- Scroll Up/Down add class --*/
+// var lastScrollTop = 0;
+// $(window).scroll(function(event){
+//    var st = $(this).scrollTop();
+//    if (st > lastScrollTop){
+// 	   $('.nav-item').addClass('scrolling_down');
+//        $('.nav-item').removeClass('scrolling_up');
+//        $('.nav-wrap').removeClass("nav-fixed");
+//    } else {
+// 	   $('.nav-item').addClass('scrolling_up');
+//        $('.nav-item').removeClass('scrolling_down');
+//        $('.nav-wrap').addClass("nav-fixed")
+//    }
+//    lastScrollTop = st;
+// });	
+
+/*-- hover games menu --*/
+// $(document).ready(function(){
+//     $('.main-nav').mouseover(function(){
+//         $('.is_hover').removeClass("is_hover");
+//         $('.nav-item').addClass('scrolling_down');
+//         $('.main-nav').addClass("is_hover");
+//         $('.nav-wrap').addClass("nav-fixed")
+//         console.log("is_hover")
+//         return false;
+//     });
+//     $('.main-nav').mouseleave(function(){
+//         $('.main-nav').removeClass("is_hover");
+//         $('.nav-wrap').removeClass("nav-fixed");
+//     });
+//         });
+
+/*--  hover games menu to open submenu --*/
+// $('#gamesHover, .games-menu').hover(function() {
+//     $('.games-menu').show();
+// }, function() {
+//     $('.games-menu').hide();
+// });
+
+
 //hover games menu bar
 
-$("#gamesHover").hover(
-function () {
-        $('.games-menu').addClass('games-hover');
-        $('#nav-area').removeClass('nav-scroll');
-        $('.nav-wrap').addClass('nav-black');
-}, 
-function () {
-    $('.games-menu').removeClass('games-hover');
-}
-);
+// $("#gamesHover").hover(
+// function () {
+//         $('.games-menu').addClass('games-hover');
+//         $('#nav-area').removeClass('nav-scroll');
+//         $('.nav-wrap').addClass('nav-black');
+// }, 
+// function () {
+//     $('.games-menu').removeClass('games-hover');
+// }
+// );
 
 
 
 
 
-//navbar scroll add class
-$(window).on('scroll', function(){
-    if($(window).scrollTop()){
-        $('#nav-area').addClass('nav-scroll');
-        $('.nav-wrap').addClass('nav-black');
-    }
-    else{
-        $('#nav-area').removeClass('nav-scroll');
-        $('.nav-wrap').removeClass('nav-black');
-    }
-})
+
 
 //hide nav menu for search
 $(document).ready(function(){
