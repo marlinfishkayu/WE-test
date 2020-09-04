dbc49b0a3d1227bcb2b14b38fada2bdffe7dc524
marlinfishkayu <69189573+marlinfishkayu@users.noreply.github.com>
2020-09-04 22:15:59 +0800

    u

--- js/main.js
+++ js/main.js
@@ -284,3 +284,21 @@ $('#StudioSlider').lightSlider({
     ]
 });
 
+
+/*-- All Live Game popular game slider--*/
+$('.popular-game-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
