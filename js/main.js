f035197c57df284fb6736a7a6023602bb10745ed
marlinfishkayu <69189573+marlinfishkayu@users.noreply.github.com>
2020-09-11 12:23:19 +0800

    u

--- js/main.js
+++ js/main.js
@@ -395,3 +395,25 @@ $(function(){
       $('#evnet-page').html(item+" / "+items)
     }
     });
+
+ /*-- Number counting animation --*/
+// const counters = document.querySelectorAll('.counter');
+// const speed = 500;
+
+// counters.forEach(counter =>{
+//   const updateCount = () =>{
+//     const target = +counter.getAttribute('data-target');
+//     const count = +counter.innerText;
+
+//     const inc = target / speed;
+
+//     if(count < target){
+//       counter.innerText = Math.ceil(count + inc);
+//       setTimeout(updateCount,1);
+//     } else{
+//       count.innerText = target;
+//     }
+//   }
+
+//   updateCount();
+// })
