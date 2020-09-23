ed0631567238cba3a5c4f7900a7d2c18987666b7
marlinfishkayu <69189573+marlinfishkayu@users.noreply.github.com>
2020-09-23 18:00:41 +0800

    u

--- js/main.js
+++ js/main.js
@@ -467,4 +467,17 @@ return $(this).css('color', '#434343');
 
 
 
-/*--  select placeholder --*/
+/*--  cookie popup --*/
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
