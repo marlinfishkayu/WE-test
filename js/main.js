e873ac3fdc70fa114a71d58cef43da8d62bd371d
marlinfishkayu <69189573+marlinfishkayu@users.noreply.github.com>
2020-10-09 13:08:40 +0800

    up

--- js/main.js
+++ js/main.js
@@ -422,17 +422,17 @@ $(function(){
       onTranslated : counter //When the translation of the stage has finished.
     });
     
-    // function counter(event) {
-    //    var element   = event.target;         // DOM element, in this example .owl-carousel
-    //     var items     = event.item.count;     // Number of items
-    //     var item      = event.item.index + 1;     // Position of the current item
+    function counter(event) {
+       var element   = event.target;         // DOM element, in this example .owl-carousel
+        var items     = event.item.count;     // Number of items
+        var item      = event.item.index + 1;     // Position of the current item
       
-    //   // it loop is true then reset counter from 1
-    //   if(item > items) {
-    //     item = item - items
-    //   }
-    //   $('#evnet-page').html(item+" / "+items)
-    // }
+      // it loop is true then reset counter from 1
+      if(item > items) {
+        item = item - items
+      }
+      $('#evnet-page').html(item+" / "+items)
+    }
     });
 
  /*-- Number counting animation --*/
