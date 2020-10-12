bc1faf4dfc6ac44fe5fc110c2539df0f3d37919b
marlinfishkayu <69189573+marlinfishkayu@users.noreply.github.com>
2020-10-12 15:34:36 +0800

    up

--- js/main.js
+++ js/main.js
@@ -548,20 +548,23 @@ $('#signUp').click(function(){
 //     cookieContainer.classList.add("active");
 //   };
 // }, 1000);
-// const cookieContainer = document.querySelector(".cookie-container");
-// const cookieButton = document.querySelector(".cookie-btn");
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
 
 
-// cookieButton.addEventListener("click", () => {
-//   cookieContainer.classList.remove("active");
-//   localStorage.setItem("cookieBannerDisplayed", "true");
-// });
+cookieButton.addEventListener("click", function() {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
 
-// setTimeout(() => {
-//   if (!localStorage.getItem("cookieBannerDisplayed")) {
-//     cookieContainer.classList.add("active");
-//   };
-// }, 1000);
+setTimeout(function() {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  };
+}, 1000);
+
+
 
 
 /*--  cookie popup test 1 --*/
