559de18b9124606ac56d48de4adf109ac20a776e
marlinfishkayu <69189573+marlinfishkayu@users.noreply.github.com>
2020-10-21 17:56:16 +0800

    up

--- json/multi-language/lang_index.js
+++ json/multi-language/lang_index.js
@@ -3,6 +3,9 @@ var arrLang = {
       'nav_About': 'AboutWE',
       'nav_LiveGames': 'Games',
       'nav_LiveStudio': 'Live Studio',
+      'nav_LiveServices': 'Live Services',
+      'nav_WhatsNew': "What's New",
+      'nav_ContactUs': 'Contact Us',
 
 
 
@@ -14,6 +17,9 @@ var arrLang = {
       'nav_About': '關於WE',
       'nav_LiveGames': '遊戲',
       'nav_LiveStudio': '攝影棚',
+      'nav_LiveServices': '服務',
+      'nav_WhatsNew': "最新消息",
+      'nav_ContactUs': "聯絡我們",
 
       'current-lang': '中'
       
