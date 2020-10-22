caf25c9f68cffafb792682712a69be8e2e2790f2
marlinfishkayu <69189573+marlinfishkayu@users.noreply.github.com>
2020-10-22 16:22:38 +0800

    up

--- json/multi-language/lang_index.js
+++ json/multi-language/lang_index.js
@@ -1,21 +1,35 @@
 var arrLang = {
     'en': {
       //navbar
+      'current-lang': 'En',
       'nav_About': 'AboutWE',
       'nav_LiveGames': 'Games',
       'nav_LiveStudio': 'Live Studio',
       'nav_LiveServices': 'Live Services',
       'nav_WhatsNew': "What's New",
       'nav_ContactUs': 'Contact Us',
+      'nav_Search': 'Search our website',
       //navbar-submenu
       'submenu_LiveGames': 'Live Game',
       'submenu_Lottery': 'Lottery',
       'submenu_Sportsbook': 'Sportsbook',
       'submenu_Slots': 'Slots',
-
-
-      'current-lang': 'En'
+      'submenu_AllLiveGame': 'All Live Game',
+      //livegame submenu
+      'Baccarat': 'Baccarat',
+      'Roulette': 'Roulette',
+      'SicBo': 'Sic Bo',
+      'DragonTiger': 'Dragon Tiger',
+      'MahjongLuckyWheel': 'Mahjong Lucky Wheel',
+      'SqueezeBaccarat': 'Squeeze Baccarat',
+      'GodofFortuneSicBo': 'God of Fortune Sic Bo',
+      'GodofFortuneRoulette': 'God of Fortune Roulette',
+      'WinThreeCards': 'Win Three Cards',
       
+      //index_hero_slider
+      'index_slider1_h2': 'WE are more than fun',
+      'index_slider1_p': 'Way to go, lit the sparkle of innovation.',
+      'tryNow': 'try now',
     },
     'cn': {
       //navbar
@@ -23,16 +37,31 @@ var arrLang = {
       'nav_LiveGames': '遊戲',
       'nav_LiveStudio': '攝影棚',
       'nav_LiveServices': '服務',
-      'nav_WhatsNew': "最新消息",
-      'nav_ContactUs': "聯絡我們",
+      'nav_WhatsNew': '最新消息',
+      'nav_ContactUs': '聯絡我們',
+      'nav_Search': '搜索我們的網站',
+      'current-lang': '中',
       //navbar-submenu
       'submenu_LiveGames': '真人遊戲',
       'submenu_Lottery': '彩票',
       'submenu_Sportsbook': '體育博彩',
       'submenu_Slots': '老虎機',
+      'submenu_AllLiveGame': '顯示全部',
+      //livegame submenu
+      'Baccarat': '百家樂',
+      'Roulette': '輪盤',
+      'SicBo': '骰寶',
+      'DragonTiger': '龍虎',
+      'MahjongLuckyWheel': '麻將幸運輪',
+      'SqueezeBaccarat': '共咪百家樂',
+      'GodofFortuneSicBo': '財神骰寶',
+      'GodofFortuneRoulette': '財神輪盤',
+      'WinThreeCards': '炸金花',
 
-
-      'current-lang': '中'
+      //index_hero_slider
+      'index_slider1_h2': '這裡，讓你心花怒放',
+      'index_slider1_p': '比好玩 · 更好玩',
+      'tryNow': '試玩',
       
     }
   };
