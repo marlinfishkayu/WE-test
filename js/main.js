6948234244b63f67cdde5ede719fc44d1780c4e6
marlinfishkayu <69189573+marlinfishkayu@users.noreply.github.com>
2020-10-30 16:11:43 +0800

    up

--- js/main.js
+++ js/main.js
@@ -172,6 +172,7 @@ $(document).ready(function(){
         $('.closeBtn').removeClass('active')
         $('.nav-wrap').removeClass('search-black')
         $('.search-container').removeClass('search-block');
+        $('.search-input').val("");
     })
 });
 
